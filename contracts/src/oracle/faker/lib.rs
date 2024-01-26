0143dd4397d7a71a931564e195aa68ed82583a8c
caglavol <volkan@1delta.io>
2024-01-26 14:14:48 +0100

    add fake oracle for e2e tests

---
+++ contracts/src/oracle/faker/lib.rs
@@ -0,0 +1,52 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod faker {
+    use dia_oracle_getter::OracleGetters;
+    use ink::prelude::string::String;
+    use ink::prelude::vec::Vec;
+    use ink::storage::Mapping;
+
+    #[ink(storage)]
+    pub struct Faker {
+        oracle: Mapping<String, (u64, u128)>,
+    }
+
+    impl Faker {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {
+                oracle: Default::default()
+            }
+        }
+
+        #[ink(message)]
+        pub fn set_price(&mut self, pair: String, price: u128) -> () {
+            self.oracle.insert(pair, &(0, price));
+        }
+    }
+
+    impl OracleGetters for Faker {
+        #[ink(message)]
+        fn get_updater(&self) -> AccountId {
+            AccountId::from([0; 32])
+        }
+    
+        // price has 18 decimals
+        #[ink(message)]
+        fn get_latest_price(&self, pair: String) -> Option<(u64, u128)> {
+            self.oracle.get(pair)
+        }
+    
+        #[ink(message)]
+        fn get_latest_prices(&self, pairs: Vec<String>) -> Vec<Option<(u64, u128)>> {
+            let mut prices = Vec::new();
+
+            for pair in pairs {
+                prices.push(self.oracle.get(pair));
+            }
+
+            prices
+        }
+    }
+}
