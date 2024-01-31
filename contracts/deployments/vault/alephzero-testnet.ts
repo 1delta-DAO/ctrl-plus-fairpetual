992eb0c63b7fc864e50a23012f9a72dba9d9471a
caglavol <volkan@1delta.io>
2024-01-31 02:40:40 +0100

    update contracts

--- contracts/deployments/vault/alephzero-testnet.ts
+++ contracts/deployments/vault/alephzero-testnet.ts
@@ -1,2 +1,2 @@
-export const address = '5CTBRjvHeC5xZXiv2Y9RPRCgkqajaYydCXcnS2d2tJqtX8TA'
-export const blockNumber = 53260922
+export const address = '5DCgqXnr2PRhEY43zZWeE4imamp337ax5WH2S9W1rqaiAABv'
+export const blockNumber = 53277175
