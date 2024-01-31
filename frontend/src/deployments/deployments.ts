c142c6ed1192fc1037b1d3fe8d12e51b8a3c93cd
caglavol <volkan@1delta.io>
2024-01-31 01:56:29 +0100

    fix missing project name change

--- frontend/src/deployments/deployments.ts
+++ frontend/src/deployments/deployments.ts
@@ -17,9 +17,9 @@ export const getDeployments = async (): Promise<SubstrateDeployment[]> => {
 
   for (const networkId of networks) {
     for (const contractId of Object.values(ContractIds)) {
-      const abi = await import(`@inkathon/contracts/deployments/${contractId}/${contractId}.json`)
+      const abi = await import(`@fairpetuals/contracts/deployments/${contractId}/${contractId}.json`)
       const { address } = await import(
-        `@inkathon/contracts/deployments/${contractId}/${networkId}.ts`
+        `@fairpetuals/contracts/deployments/${contractId}/${networkId}.ts`
       )
 
       deployments.push({ contractId, networkId, abi, address })
